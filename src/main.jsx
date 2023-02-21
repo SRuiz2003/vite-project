0c294c79cb9012f35517e31f52c94d550783a0a9
SRuiz2003 <123700454+SRuiz2003@users.noreply.github.com>
2023-02-21 14:04:15 -0500

    Update main.jsx
    SecToHr summon

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,11 @@ import App from './App'
 import './index.css'
 import {CategoryList}  from './components/CategoryList'
 import {GifGrid} from './components/GifGrid'
+import { SecToHr } from './components/SecToHr'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SecToHr/>
     <GifGrid/>
     <CategoryList/>
     <App />
